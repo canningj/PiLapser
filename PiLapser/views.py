aaeda5913b0b7e2585f9b1a3fcc8d40b18d51863
canning87 <majorleaguedesign@gmail.com>
2017-10-23 01:05:30 -0600

    Issue with formatting

--- PiLapser/views.py
+++ PiLapser/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from piLapse import moveForward
+from .piLapse import moveForward
 
 from .forms import totalImages
 
