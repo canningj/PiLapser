71e99348c921397177a42c063d30cff264dec37e
canning87 <majorleaguedesign@gmail.com>
2017-10-23 01:30:26 -0600

    Fixing urls

--- PiLapser/urls.py
+++ PiLapser/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from PiLapser import views
 
 urlpatterns = [
+	url(r'^piLapse/$', views.getTotalImages, name='getTotalImages'),
 	url(r'^$', views.HomePageView.as_view()),
 ]
