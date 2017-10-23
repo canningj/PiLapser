71ca9c951da323a2636d3432ce96e2619711fcdd
canning87 <majorleaguedesign@gmail.com>
2017-10-23 02:16:47 -0600

    Removed name tag for form field

--- PiLapser/forms.py
+++ PiLapser/forms.py
@@ -4,4 +4,4 @@ class totalImages(forms.Form):
     total_images = forms.IntegerField(label='# of photos: ')
 
 class stepCount(forms.Form):
-    step_count = forms.IntegerField(label="# of Steps: ", name="step count")
+    step_count = forms.IntegerField(label="# of Steps: ")
